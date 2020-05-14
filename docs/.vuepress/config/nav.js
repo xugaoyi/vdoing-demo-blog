9222aa29b28f64fa46cdc066bc597a2cfdc4fbff
xugaoyi <894072666@qq.com>
2020-05-14 16:50:10 +0800

    init

---
+++ docs/.vuepress/config/nav.js
@@ -0,0 +1,8 @@
+module.exports = [
+  {text: '首页', link: '/'},
+  {text: '分类', link: '/categories/'},
+  {text: '标签', link: '/tags/'},
+  {text: '关于', link: '/about/'},
+  {text: '友接', link: '/friends/'},
+  {text: '归档', link: '/archives/'}
+]
